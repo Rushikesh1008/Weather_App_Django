97f2bcd3a85327a771c3c2f291478c6eb2ae0b72
Rushikesh Shendare <rushidamail@gmail.com>
2020-05-12 00:24:53 +0530

    Capitalize city objects

--- weather_app/weather/models.py
+++ weather_app/weather/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class CityField(models.CharField):
+    def to_python(self, value):
+        return value.capitalize()
+
 class City(models.Model):
-    name = models.CharField(max_length = 25,unique=True)
+    name = CityField(max_length = 25,unique=True)
 
     def __str__(self):
         return self.name
--- weather_app/weather_app/settings.py
+++ weather_app/weather_app/settings.py
@@ -126,3 +126,4 @@ try:
     from .local_settings import *
 except ImportError:
     pass
+    
